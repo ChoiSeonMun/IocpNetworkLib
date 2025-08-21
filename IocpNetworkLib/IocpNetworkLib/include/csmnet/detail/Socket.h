eac379f18a88cda60e1bbca88e33251ad705d8ca
smchoi <seonmun.choi@gmail.com>
2025-08-21 11:30:00 +0900

    feat: Socket 구현

---
+++ IocpNetworkLib/IocpNetworkLib/include/csmnet/detail/Socket.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Config.h"
+#include "SocketOption.h"
+#include "IIocpRegistrable.h"
+#include "csmnet/Endpoint.h"
+
+#include <span>
+
+namespace csmnet::detail
+{
+    using namespace std;
+
+    enum class SocketType
+    {
+        Tcp,
+        Udp
+    };
+
+    class AcceptEvent;
+    class ConnectEvent;
+
+    enum class SocketState
+    {
+        Closed,
+        Opened,
+        Bound,
+        Listening,
+        Connected
+    };
+
+    class Socket final : public IIocpRegistrable
+    {
+    public:
+        using Linger = Linger<SOL_SOCKET, SO_LINGER>;
+        using ReuseAddress = Boolean<SOL_SOCKET, SO_REUSEADDR>;
+        using UpdateAcceptContext = UpdateAcceptContext<SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT>;
+
+        Socket() noexcept = default;
+        Socket(const Socket&) = delete;
+        Socket& operator=(const Socket&) = delete;
+        Socket(Socket&& other) noexcept;
+        Socket& operator=(Socket&& other) noexcept;
+        ~Socket() noexcept
+        {
+            Close();
+        }
+
+        bool IsOpen() const noexcept { return _state != SocketState::Closed; }
+        expected<void, error_code> Open(SocketType type) noexcept;
+        void Close() noexcept;
+
+        expected<void, error_code> Bind(const Endpoint& local) noexcept;
+        expected<void, error_code> Listen(int32 backlog = SOMAXCONN) noexcept;
+        expected<void, error_code> AcceptEx(AcceptEvent& event) noexcept;
+        expected<void, error_code> ConnectEx(const Endpoint& remote, ConnectEvent& event) noexcept;
+
+        template <typename SocketOption>
+        void SetOption(const SocketOption& option) noexcept
+        {
+            ::setsockopt(_socket, option.GetLevel(), option.GetName(), reinterpret_cast<const char*>(option.GetData()), option.GetSize());
+        }
+
+        operator SOCKET() const noexcept { return _socket; }
+        
+        HANDLE GetHandle() const noexcept override { return reinterpret_cast<HANDLE>(_socket); }
+    private:
+        SOCKET _socket = INVALID_SOCKET;
+        SocketState _state = SocketState::Closed;
+    };
+}
---
+++ IocpNetworkLib/IocpNetworkLib/src/Socket.cpp
@@ -0,0 +1,164 @@
+#include "pch.h"
+#include "csmnet/detail/Socket.h"
+#include "csmnet/Endpoint.h"
+#include "csmnet/detail/IocpEvent.h"
+
+using namespace std;
+
+namespace csmnet::detail
+{
+    expected<void, error_code> Socket::Open(SocketType type) noexcept
+    {
+        if (IsOpen())
+        {
+            return unexpected(LibError::SocketAlreadyOpen);
+        }
+
+        switch (type)
+        {
+        case SocketType::Tcp:
+            _socket = ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
+            break;
+        case SocketType::Udp:
+            _socket = ::WSASocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP, nullptr, 0, WSA_FLAG_OVERLAPPED);
+            break;
+        default:
+            break;
+        }
+
+        if (_socket == INVALID_SOCKET)
+        {
+            return unexpected(TranslateWsaError(WSAGetLastError()));
+        }
+
+        _state = SocketState::Opened;
+        return {};
+    }
+
+    void Socket::Close() noexcept
+    {
+        if (IsOpen())
+        {
+            ::closesocket(_socket);
+            _socket = INVALID_SOCKET;
+            _state = SocketState::Closed;
+        }
+    }
+
+    Socket::Socket(Socket&& other) noexcept
+        :_socket(exchange(other._socket, INVALID_SOCKET)),
+        _state(exchange(other._state, SocketState::Closed))
+    {
+    }
+
+    Socket& Socket::operator=(Socket&& other) noexcept
+    {
+        if (this != &other)
+        {
+            Close();
+            _socket = exchange(other._socket, INVALID_SOCKET);
+            _state = exchange(other._state, SocketState::Closed);
+        }
+
+        return *this;
+    }
+
+    expected<void, error_code> Socket::Bind(const Endpoint& local) noexcept
+    {
+        if (IsOpen() == false)
+        {
+            return unexpected(LibError::SocketNotOpen);
+        }
+
+        int32 result = ::bind(_socket, local.GetNative(), local.GetSize());
+        if (result == SOCKET_ERROR)
+        {
+            return unexpected(TranslateWsaError(WSAGetLastError()));
+        }
+
+        _state = SocketState::Bound;
+        return {};
+    }
+
+    expected<void, error_code> Socket::Listen(int32 backlog) noexcept
+    {
+        if (IsOpen() == false)
+        {
+            return unexpected(LibError::SocketNotOpen);
+        }
+
+        if (_state != SocketState::Bound)
+        {
+            return unexpected(LibError::SocketNotBound);
+        }
+
+        int32 result = ::listen(_socket, backlog);
+        if (result == SOCKET_ERROR)
+        {
+            return unexpected(TranslateWsaError(WSAGetLastError()));
+        }
+
+        _state = SocketState::Listening;
+        return {};
+    }
+
+    expected<void, error_code> Socket::AcceptEx(AcceptEvent& event) noexcept
+    {
+        if (IsOpen() == false)
+        {
+            return unexpected(LibError::SocketNotOpen);
+        }
+
+        if (_state != SocketState::Listening)
+        {
+            return unexpected(LibError::SocketNotListening);
+        }
+
+        BOOL result = ::AcceptEx(_socket,
+            event.GetAcceptSocket(),
+            event.GetBuffer(),
+            0,
+            sizeof(sockaddr_in) + 16,
+            sizeof(sockaddr_in) + 16,
+            reinterpret_cast<LPDWORD>(event.GetBytesTransferredData()),
+            event.GetOverlapped());
+
+        if (result == FALSE && ::WSAGetLastError() != WSA_IO_PENDING)
+        {
+            return unexpected(TranslateWsaError(WSAGetLastError()));
+        }
+
+        return {};
+    }
+
+    expected<void, error_code> Socket::ConnectEx(const Endpoint& remote, ConnectEvent& event) noexcept
+    {
+        if (IsOpen() == false)
+        {
+            return unexpected(LibError::SocketNotOpen);
+        }
+
+        if (_state != SocketState::Bound)
+        {
+            return unexpected(LibError::SocketNotBound);
+        }
+
+        BOOL result = WinsockExtension::ConnectEx(
+            _socket,
+            remote.GetNative(),
+            remote.GetSize(),
+            nullptr,
+            0,
+            reinterpret_cast<LPDWORD>(event.GetBytesTransferredData()),
+            event.GetOverlapped()
+        );
+
+        if (result == FALSE && ::WSAGetLastError() != WSA_IO_PENDING)
+        {
+            return unexpected(TranslateWsaError(WSAGetLastError()));
+        }
+
+        _state = SocketState::Connected;
+        return {};
+    }
+}
