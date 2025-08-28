d736a77ed3537d43cd8037c7d1baef271ddd1b21
smchoi <seonmun.choi@gmail.com>
2025-08-28 20:46:04 +0900

    refactor(Endpoint.h): 생성자의 보호 수준 변경

--- IocpNetworkLib/IocpNetworkLib/include/csmnet/Endpoint.h
+++ IocpNetworkLib/IocpNetworkLib/include/csmnet/Endpoint.h
@@ -13,14 +13,14 @@ namespace csmnet
         static Endpoint Any(uint16 port) noexcept;
         static optional<Endpoint> From(std::string_view ip, uint16 port) noexcept;
 
+        explicit Endpoint(sockaddr_in sockaddr) noexcept : _addr(sockaddr) {}
+
         std::string GetIp() const;
         uint16 GetPort() const noexcept { return ::ntohs(_addr.sin_port); }
         sockaddr* GetNative() noexcept { return reinterpret_cast<sockaddr*>(&_addr); }
         const sockaddr* GetNative() const noexcept { return reinterpret_cast<const sockaddr*>(&_addr); }
         size_t GetSize() const noexcept { return sizeof(_addr); }
         socklen_t GetCapacity() const noexcept { return sizeof(_addr); }
-    private:
-        Endpoint(sockaddr_in sockaddr) noexcept : _addr(sockaddr) { }
     private:
         sockaddr_in _addr;
     };
