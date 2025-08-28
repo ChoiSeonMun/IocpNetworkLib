3ca54e0d5db6346331e840f7c602e7dfb9572650
smchoi <seonmun.choi@gmail.com>
2025-08-28 19:08:17 +0900

    refactor(ObjectPool.h): shared_ptr로 PooledObject 구현

--- IocpNetworkLib/IocpNetworkLib/include/csmnet/util/ObjectPool.h
+++ IocpNetworkLib/IocpNetworkLib/include/csmnet/util/ObjectPool.h
@@ -9,68 +9,23 @@
 namespace csmnet::util
 {
     template <typename T>
-    class PooledObject
-    {
-    public:
-        PooledObject() noexcept = default;
-        PooledObject(T* obj, class ObjectPool<T>* pool) noexcept
-            : _object(obj), _pool(pool) {
-        }
-        PooledObject(const PooledObject&) = delete;
-        PooledObject& operator=(const PooledObject&) = delete;
-        PooledObject(PooledObject&& other) noexcept
-            : _object(std::exchange(other._object, nullptr)),
-            _pool(std::exchange(other._pool, nullptr))
-        {
-        }
-        PooledObject& operator=(PooledObject&& other) noexcept
-        {
-            if (this != &other)
-            {
-                Reset();
-                _object = std::exchange(other._object, nullptr);
-                _pool = std::exchange(other._pool, nullptr);
-            }
-            return *this;
-        }
-        ~PooledObject() noexcept
-        {
-            Reset();
-        }
-
-
-        T* operator->() const noexcept { return _object; }
-        T& operator*() const noexcept { return *_object; }
-        T* Get() const noexcept { return _object; }
-
-        explicit operator bool() const noexcept { return _object != nullptr; }
-        bool IsValid() const noexcept { return _object != nullptr; }
-
-        void Reset() noexcept
-        {
-            if (_object && _pool)
-            {
-                _pool->Push(_object);
-                _object = nullptr;
-                _pool = nullptr;
-            }
-        }
-    private:
-        T* _object = nullptr;
-        class ObjectPool<T>* _pool = nullptr;
-    };
+    using PooledObject = std::shared_ptr<T>;
 
     template <typename T, typename Container = std::vector<T>>
     class ObjectPool
     {
-        friend class PooledObject<T>;
     public:
+        using PooledObject = util::PooledObject<T>;
         using ResetAction = std::function<void(T*)>;
     public:
         ObjectPool(size_t poolSize, ResetAction resetAction)
-            : _container(poolSize), _resetAction(resetAction)
+            :_resetAction(resetAction),
+            _container(poolSize)
         {
-            _pool.push_range(_container);
+            for (auto& obj : _container)
+            {
+                _pool.push(&obj);
+            }
         }
         ObjectPool(const ObjectPool&) = delete;
         ObjectPool& operator=(const ObjectPool&) = delete;
@@ -78,27 +33,30 @@ namespace csmnet::util
         ObjectPool& operator=(ObjectPool&&) noexcept = default;
         ~ObjectPool() noexcept = default;
 
-        PooledObject<T> Pop() noexcept
+        // 풀이 비었다면 nullptr일 수 있다.
+        PooledObject Pop() noexcept
         {
             if (_pool.empty())
             {
-                return {};
+                return nullptr;
             }
 
             T* obj = _pool.top();
             _pool.pop();
             _resetAction(obj);
-            
-            return PooledObject<T>(obj, this);
+
+            auto deleter = [this](T* p) { Push(p); };
+
+            return PooledObject(obj, deleter);
         }
     private:
-        void Push(T* object)
+        void Push(T* object) noexcept
         {
             _pool.push(object);
         }
     private:
         ResetAction _resetAction;
         Container _container;
-        std::stack<T*> _pool;
+        std::stack<T*, std::vector<T*>> _pool;
     };
 }
